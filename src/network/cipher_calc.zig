47b13afbfd3b38004ce225a24e6dfcdb38414fcd
lkrumov <lkrumov@example.org>
2025-02-08 00:20:22 +0200

    feat: Basic packet decryption

--- src/network/cipher_calc.zig
+++ src/network/cipher_calc.zig
@@ -1,56 +1,95 @@
+const std = @import("std");
+const print = std.debug.print;
+const allocator = std.heap.page_allocator;
+const keys = @import("keys.zig");
+
 const bits_per_byte: u32 = 8;
 const bits_per_value: u32 = bits_per_byte * 2 + 2;
 
 pub const CipherBlocks = struct {
     encrypted: u8,
     decrypted: u8,
-    encryption: u8,
+    unmasked: u8,
 };
 
-pub const CipherVersion = enum(u32) {
-    New,
-    Old
-};
+pub const CipherVersion = enum(u16) { New, Old };
 
-pub fn get_blocks(version: CipherVersion) CipherBlocks {
+pub fn split_into_blocks(packet: []const u8, size: usize) ![][]u8 {
+    const blocks = try allocator.alloc([]u8, packet.len / size);
+
+    for (blocks, 0..) |*chunk, i| {
+        const start = i * size;
+        const end = start + size;
+        chunk.* = try allocator.alloc(u8, size);
+        std.mem.copyForwards(u8, chunk.*, packet[start..end]);
+    }
+
+    return blocks;
+}
+
+pub fn block_alloc(T: type, size: usize) ![]T {
+    return try allocator.alloc(T, size);
+}
+
+pub fn get_checksum(block: []u64) u64 {
+    var checksum: u64 = keys.checksum_xor_key;
+    for (0..block.len) |i| {
+        checksum ^= block[i];
+    }
+    return checksum;
+}
+
+pub fn get_block_size(block_suffix: [2]u8) u8 {
+    return block_suffix[0] ^ block_suffix[1] ^ keys.block_size_xor_key;
+}
+
+pub fn get_block_sizes(version: CipherVersion) CipherBlocks {
     return switch (version) {
-        CipherVersion.New => CipherBlocks
-        {
-            .decrypted = 8,
-            .encrypted = 11,
-            .encryption = 4
-        },
-        else => CipherBlocks
-        {
-            .decrypted = 32,
-            .encrypted = 38,
-            .encryption = 16
-        }
+        CipherVersion.New => CipherBlocks{ .decrypted = 8, .encrypted = 11, .unmasked = 4 },
+        else => CipherBlocks{ .decrypted = 32, .encrypted = 38, .unmasked = 16 },
     };
-} 
-
-pub fn get_byte_offset(idx: u32) u32 {
-    return get_bit_index(idx) / bits_per_byte;
 }
 
-pub fn get_bit_offset(idx: u32) u32 {
-    return get_bit_index(idx) % bits_per_byte;
+// Returns 4 bits
+pub fn get_byte_offset(idx: usize) u4 {
+    return @intCast(get_bit_index(idx) / bits_per_byte);
 }
 
-pub fn get_first_bit_mask(idx: u32) u32 {
-    return 0xFF >> get_bit_offset(idx);
+// Max % is 9
+pub fn get_bit_offset(idx: usize) u4 {
+    return @intCast(get_bit_index(idx) % bits_per_byte);
 }
 
-pub fn get_remainder_bit_mask(idx: u32) u32 {
-    return (0xFF << 6 - get_bit_offset(idx) & 0xFF) - (0xFF << 8 - get_bit_offset(idx) & 0xFF);
+// Returns 8 bits
+pub fn get_first_bit_mask(idx: usize) u9 {
+    const bit_offset = get_bit_offset(idx);
+    const mask: u9 = 0xFF;
+    return (mask >> bit_offset);
 }
 
-pub fn get_bit_index(idx: u32) u32 {
-    return idx * bits_per_value;
+pub fn get_remainder_bit_mask(idx: usize) u32 {
+    const mask: u32 = 0xFF;
+    const first_shift_offset: u5 = @intCast(6 - get_bit_offset(idx));
+    const second_shift_offset: u5 = @intCast(8 - get_bit_offset(idx));
+    return (mask << first_shift_offset & mask) - (mask << second_shift_offset & mask);
+}
+
+pub fn get_bit_index(idx: usize) u8 {
+    const idx_u32: u4 = @intCast(idx);
+    return @intCast(idx_u32 * bits_per_value);
+}
+
+pub fn get_max_decrypted_size(packet: []const u8, block_size: CipherBlocks) u32 {
+    const content_size = get_payload_size(packet);
+    return content_size * block_size.decrypted / block_size.encrypted + get_header_size(packet);
+}
+
+pub fn get_payload_size(packet: []const u8) u32 {
+    return get_size(packet) - get_header_size(packet);
 }
 
 pub fn get_content_size(packet: []const u8, counter: u32, decrypted: bool) u32 {
-    var content_size = get_packet_size(&packet) - get_content_size(&packet);
+    var content_size = get_payload_size(packet);
 
     if (counter > 0 and decrypted) {
         content_size += 1;
@@ -59,7 +98,7 @@ pub fn get_content_size(packet: []const u8, counter: u32, decrypted: bool) u32 {
     return content_size;
 }
 
-pub fn get_packet_header_size(packet: []const u8) u32 {
+pub fn get_header_size(packet: []const u8) u32 {
     return switch (packet[0]) {
         0xC1, 0xC3 => 2,
         0xC2, 0xC4 => 3,
@@ -67,10 +106,42 @@ pub fn get_packet_header_size(packet: []const u8) u32 {
     };
 }
 
-pub fn get_packet_size(packet: []const u8) u32 {
+pub fn get_size(packet: []const u8) u32 {
     return switch (packet[0]) {
         0xC1, 0xC3 => packet[1],
-        0xC2, 0xC4 => packet[1] << 8 | packet[2],
+        0xC2, 0xC4 => @as(u32, packet[1]) << 8 | packet[2],
         else => 0,
     };
+}
+
+pub fn print_blocks(blocks: [][]u8) void {
+    for (blocks) |block| {
+        print_bytes(block);
+    }
+}
+
+pub fn print_bytes(bytes: []const u8) void {
+    std.debug.print("\n", .{});
+    std.debug.print("---------------------------------------------------\n", .{});
+    std.debug.print("BYTES BYTES ({d})\n", .{bytes.len});
+    std.debug.print("---------------------------------------------------\n", .{});
+    var chunk_idx: u32 = 0;
+    for (bytes, 1..) |byte, i| {
+        std.debug.print("0x{x:0>2} ", .{byte});
+        if (i % 8 == 0) {
+            print(" [{d}]\n", .{chunk_idx});
+            chunk_idx += 1;
+        }
+    }
+    if (bytes.len % 8 != 0) {
+        std.debug.print("[{d}]\n", .{chunk_idx});
+    }
+    std.debug.print("--------------------------------------------------\n", .{});
+}
+
+pub fn reverse_endianness(x: u64) u64 {
+    return (x & 0xFF000000) >> 24 |
+        (x & 0x00FF0000) >> 8 |
+        (x & 0x0000FF00) << 8 |
+        (x & 0x000000FF) << 24;
 }
--- src/network/decrypt.zig
+++ src/network/decrypt.zig
@@ -1,21 +1,165 @@
 const std = @import("std");
-const Keys = @import("keys.zig").Keys;
+const keys = @import("keys.zig");
 const cipher = @import("cipher_calc.zig");
+const allocator = std.heap.page_allocator;
+const print = std.debug.print;
 
-pub fn decrypt(packet: []const u8, decrypt_keys: Keys) []const u32 {
-    const block_sizes = cipher.get_blocks(cipher.CipherVersion.New);
+pub const DecryptError = error{ ContentSizeMismatch, ChecksumMismatch, InvalidBlockSize };
 
-    _ = block_sizes;
+pub fn decrypt(packet: []const u8, decrypt_keys: keys.Keys) ![]const u8 {
+    switch (packet[0]) {
+        // 0xC1 and 0xC2 packets are not encrypted
+        0xC1, 0xC2 => return packet,
+        else => {},
+    }
 
-    const first_byte = @as(u32, packet[0]) + decrypt_keys.key[0];
-    const result = [8] u32{ first_byte, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08 };
+    // TODO: Counter is uknow to me right now. Works by only setting it here, though...
+    // Right now it just takes 1 from the header_size, accounting for the CipherVersion.New
+    const counter: u32 = 1;
+    const header = packet[0..3];
 
-    return &result;
+    const content_size = cipher.get_content_size(packet, counter, false);
+    const block_size = cipher.get_block_sizes(cipher.CipherVersion.New);
+
+    if (content_size % block_size.encrypted != 0) {
+        return error.ContentSizeMismatch;
+    }
+
+    const max_size = cipher.get_max_decrypted_size(packet, block_size);
+    const header_size = cipher.get_header_size(packet);
+    const payload_offset = header_size - counter;
+
+    const encrypted_packet = packet[header_size..];
+    var output = try allocator.alloc(u8, max_size);
+    const encrypted_blocks = try cipher.split_into_blocks(encrypted_packet, block_size.encrypted);
+
+    var block_offset: u32 = 0;
+
+    for (encrypted_blocks) |encrypted_block| {
+        const unmasked_block: []u64 = try allocator.alloc(u64, block_size.unmasked);
+        const decrypted_block = try cipher.block_alloc(u64, block_size.decrypted);
+
+        // Unmask the blocks
+        {
+            for (unmasked_block, 0..) |*value, i| {
+                value.* = 0;
+
+                const byte_offset = cipher.get_byte_offset(i);
+                const bit_offset = cipher.get_bit_offset(i);
+                const first_mask = cipher.get_first_bit_mask(i);
+
+                const first_offset: u6 = @as(u5, 24) + bit_offset;
+                value.* += @as(u64, encrypted_block[byte_offset] & first_mask) << first_offset;
+
+                const second_offset: u6 = @as(u5, 16) + bit_offset;
+                value.* += @as(u64, encrypted_block[byte_offset + 1]) << second_offset;
+
+                const final_offset: u5 = @intCast(8 + bit_offset);
+                const final_mask: u32 = 0xFF;
+                const final_mask_offset: u5 = @intCast(8 - bit_offset);
+                value.* += @as(u64, encrypted_block[byte_offset + 2] & final_mask << final_mask_offset) << final_offset;
+                value.* = cipher.reverse_endianness(value.*);
+
+                const remainder_mask = cipher.get_remainder_bit_mask(i);
+                const remainder: u32 = @intCast(encrypted_block[byte_offset + 2] & remainder_mask);
+                const remainder_adjust: u5 = @intCast(6 - bit_offset);
+                value.* += (remainder << 16) >> remainder_adjust;
+            }
+        }
+
+        // Decrypt the blocks with the XOR keys
+        {
+            var i = unmasked_block.len - 1;
+
+            while (i > 0) {
+                unmasked_block[i - 1] ^= decrypt_keys.xor_key[i - 1] ^ (unmasked_block[i] & 0xFFFF);
+                i -= 1;
+            }
+        }
+
+        // Decrypt the blocks with the XOR, Modulus and Decrypt keys
+        {
+            for (0..unmasked_block.len) |i| {
+                var result = decrypt_keys.xor_key[i] ^ unmasked_block[i] * decrypt_keys.key[i] % decrypt_keys.modulus_key[i];
+
+                if (i > 0) {
+                    result ^= unmasked_block[i - 1] & 0xFFFF;
+                }
+
+                decrypted_block[2 * i] = result & 0xFF; // Lower byte
+                decrypted_block[2 * i + 1] = (result >> 8) & 0xFF; // Higher byte
+            }
+        }
+
+        const block_suffix: [2]u8 = .{ encrypted_block[encrypted_block.len - 2], encrypted_block[encrypted_block.len - 1] };
+        const computed_block_size = cipher.get_block_size(block_suffix);
+
+        // Verify the decryption
+        {
+            if (computed_block_size > decrypted_block.len) {
+                return error.InvalidBlockSize;
+            }
+
+            const checksum: u64 = cipher.get_checksum(decrypted_block);
+
+            if (block_suffix[1] != checksum) {
+                return error.ChecksumMismatch;
+            }
+        }
+
+        // Copy to output
+        {
+            const offset = payload_offset + block_offset;
+            const output_slice = output[offset .. offset + block_size.decrypted];
+
+            block_offset += computed_block_size;
+
+            // for (decrypted_block) |value| {
+            //     std.debug.print("0x{x:0>2} ", .{value});
+            // }
+            // std.debug.print("\n", .{});
+
+            for (output_slice, 0..) |*value, i| {
+                value.* = if (i < decrypted_block.len) @intCast(decrypted_block[i]) else 0;
+            }
+        }
+    }
+
+    // Format packet header
+    {
+        output[0] = header[0];
+        output = output[0 .. block_offset + header_size - counter];
+
+        switch (output[0]) {
+            0xC2, 0xC4 => {
+                output[1] = @intCast((output.len & 0xFF00) >> 8);
+                output[2] = @intCast(output.len & 0x00FF);
+            },
+            else => {
+                output[1] = @intCast(output.len);
+            },
+        }
+    }
+
+    // XOR Decrypt
+    {
+        var i = output.len - 1;
+
+        while (i > header_size) {
+            output[i] = output[i] ^ output[i - 1] ^ keys.xor32_keys[i % 32];
+            i -= 1;
+        }
+    }
+
+    return output;
 }
 
-test "packet is decrypted" {
-    const packet = [_] u8{ 1, 2, 3 };
-    const result = decrypt(&packet, @import("keys.zig").server_keys);
+test decrypt {
+    const encrypted = [_]u8{ 195, 90, 190, 36, 23, 232, 33, 50, 210, 237, 20, 177, 132, 248, 68, 13, 224, 226, 67, 1, 208, 117, 123, 78, 226, 100, 76, 187, 203, 122, 160, 178, 101, 28, 41, 15, 71, 126, 84, 195, 188, 64, 53, 57, 167, 146, 220, 162, 25, 103, 151, 62, 145, 132, 76, 134, 179, 108, 81, 32, 17, 208, 125, 112, 115, 8, 125, 72, 195, 199, 29, 110, 99, 59, 33, 206, 13, 27, 46, 211, 192, 41, 220, 86, 149, 242, 181, 44, 228, 218 };
 
-    try std.testing.expectEqual(@as(u32, 0x00007b39), result[0]);
+    const actual = try decrypt(encrypted[0..], @import("keys.zig").server_keys);
+    const expected = [_]u8{ 195, 60, 241, 1, 205, 253, 152, 252, 207, 171, 252, 207, 171, 252, 205, 253, 152, 252, 207, 171, 252, 207, 171, 252, 207, 171, 252, 207, 171, 252, 207, 171, 252, 207, 93, 116, 201, 66, 49, 48, 52, 48, 52, 107, 49, 80, 107, 50, 106, 99, 69, 84, 52, 56, 109, 120, 76, 51, 98, 0 };
+
+    try std.testing.expectEqual(expected.len, actual.len);
+    try std.testing.expect(std.mem.eql(u8, expected[0..], actual));
 }
