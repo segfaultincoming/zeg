7e7fa341db84bbd6321dcd8d37b27dc914afa372
lkrumov <lkrumov@example.org>
2025-02-09 14:05:42 +0200

    chore: Move logger out of the server file

---
+++ src/connect_server/logger.zig
@@ -0,0 +1,14 @@
+const std = @import("std");
+
+pub const LogType = enum(u1) {
+    SEND,
+    RECEIVE
+};
+
+pub fn log_bytes(packet: []const u8, log_type: LogType) void {
+    std.debug.print("{s}: ", .{@tagName(log_type)});
+    for (packet) |value| {
+        std.debug.print("0x{x:0>2} ", .{value});
+    }
+    std.debug.print("\n", .{});
+}
