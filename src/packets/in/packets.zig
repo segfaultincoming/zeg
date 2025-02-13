84772757ae25829290fc1577a384d5d784adef9b
segfaultincoming <6934369+segfaultincoming@users.noreply.github.com>
2025-02-14 00:21:57 +0200

    wip: Dynamic packet parsing with meta programming

--- src/packets/in/packets.zig
+++ src/packets/in/packets.zig
@@ -1,10 +1,14 @@
 const ServersRequest = @import("./servers_request.zig").ServersRequest;
-pub const Packet = union{
-    servers_request: ServersRequest,
 
-    pub fn from_client(self: *const Packet) []const u8 {
-        // TODO
-        _ = self;
-        return []const u8{};
-    }
+pub const Packets = union {
+    servers_request: ServersRequest,
+};
+
+pub const Packet = struct {
+    process: fn (payload: []const u8) PacketResponse,
+};
+
+pub const PacketResponse = enum {
+    Fail,
+    Success,
 };
--- src/packets/main.zig
+++ src/packets/main.zig
@@ -3,6 +3,8 @@ const network = @import("network");
 
 const types = @import("types.zig");
 const Packet = @import("in/packets.zig").Packet;
+const Packets = @import("in/packets.zig").Packets;
+const PacketResponse = @import("in/packets.zig").PacketResponse;
 
 const cipher = network.cipher;
 const keys = network.keys;
@@ -10,10 +12,10 @@ const PacketType = types.PacketType;
 const PacketHeader = types.PacketHeader;
 const PacketCode = types.PacketCode;
 
-pub fn parse(bytes: []const u8) !Packet {
+pub fn parse(bytes: []const u8) !Packets {
     const header = try header_parser(bytes);
     const codes = code_parser(header);
-    return packet_parser(codes);
+    return try handle_packet(Packets, codes);
 }
 
 pub fn header_parser(bytes: []const u8) !PacketHeader {
@@ -45,11 +47,49 @@ pub fn code_parser(packet: PacketHeader) PacketCode {
     };
 }
 
-pub fn packet_parser(packet: PacketCode) Packet {
-    // TODO
-    return Packet{
-        .servers_request = packet.payload,
+pub fn handle_packet(comptime T: type, packet: PacketCode) !PacketResponse {
+    const packets = @typeInfo(T).@"union";
+
+    inline for (packets.fields) |field| {
+        var header: PacketType = undefined;
+        var code: u8 = undefined;
+        var sub_code: u8 = undefined;
+
+        if (@hasDecl(field.type, "header")) {
+            header = field.type.header;
+        }
+
+        if (@hasDecl(field.type, "code")) {
+            code = field.type.code;
+        }
+
+        if (@hasDecl(field.type, "sub_code")) {
+            sub_code = field.type.sub_code;
+        }
+
+        if (header == packet.type and code == packet.code and sub_code == packet.sub_code) {
+            const parsed_packet = Packet{
+                .process = field.type.process,
+            };
+            return parsed_packet.process(packet.payload);
+        }
+    }
+
+    return error.PacketNotFound;
+}
+
+test handle_packet {
+    const payload = [2]u8{ 0x01, 0x02 };
+    const packet = PacketCode{
+        .type = PacketType.C1,
+        .size = 0x06,
+        .code = 0xf4,
+        .sub_code = 0x06,
+        .payload = payload[0..],
     };
+    const packet_response = try handle_packet(Packets, packet);
+
+    try std.testing.expect(packet_response == PacketResponse.Success);
 }
 
 test header_parser {
@@ -61,7 +101,7 @@ test header_parser {
 test code_parser {
     const payload = [2]u8{ 0xF1, 0xFF };
     const packet_code = code_parser(.{
-        .payload = payload[0..], 
+        .payload = payload[0..],
         .size = 0x04,
         .type = PacketType.C1,
     });
