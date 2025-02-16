6852954ea52e20a828945b55dc7bc355e91ab513
segfaultincoming <6934369+segfaultincoming@users.noreply.github.com>
2025-02-16 15:16:09 +0200

    chore: Packet generation utils

--- src/connect_server/packets/out/servers.zig
+++ src/connect_server/packets/out/servers.zig
@@ -20,40 +20,42 @@ pub const Servers = struct {
         };
     }
 
-    // NOTE: WIP, this is as beautiful as a road killed animal.
     pub fn to_client(self: *const Servers) ![]const u8 {
-        const server_len: u16 = @intCast(self.server_list.len);
-        const server_count = try utils.split_into_bytes(u16, server_len, .big);
+        const server_count = try server_count_to_client(self);
+        const servers = try server_list_to_client(self);
 
-        var servers = try utils.block_alloc(u8, server_len * 4);
-        var server_idx: usize = 0;
+        return try utils.create_packet(
+            self.header,
+            self.code,
+            self.sub_code,
+            &.{ server_count, servers },
+        );
+    }
 
-        for (
-            self.server_list.items(.id),
-            self.server_list.items(.load),
-        ) |id, load| {
-            const id_u8 = try utils.split_into_bytes(u16, id, .little);
-            const load_u8 = try utils.split_into_bytes(u16, load, .little);
-            servers[server_idx * 4 + 0] = id_u8[0];
-            servers[server_idx * 4 + 1] = id_u8[1];
-            servers[server_idx * 4 + 2] = load_u8[0];
-            servers[server_idx * 4 + 3] = load_u8[1];
-            server_idx += 1;
+    fn server_count_to_client(self: *const Servers) ![]const u8 {
+        return try utils.split_into_bytes(u16, @intCast(self.server_list.len), .big);
+    }
+
+    fn server_list_to_client(self: *const Servers) ![]const u8 {
+        var servers = try utils.block_alloc(u8, self.server_list.len * 4);
+
+        var server_offset: usize = 0;
+        for (self.server_list.items(.id), self.server_list.items(.load)) |id, load| {
+            std.mem.copyForwards(
+                u8,
+                servers[server_offset .. server_offset + 2],
+                try utils.split_into_bytes(u16, id, .little),
+            );
+            server_offset += 2;
+
+            std.mem.copyForwards(
+                u8,
+                servers[server_offset .. server_offset + 2],
+                try utils.split_into_bytes(u16, load, .little),
+            );
+            server_offset += 2;
         }
 
-        var response = std.ArrayList(u8).init(std.heap.page_allocator);
-        // (header + u16 size + code + sub_code + u16 servers_count) + severs.len
-        const servers_len: u16 = @intCast(servers.len);
-        const packet_size: u16 = 7 + servers_len;
-        const size = try utils.split_into_bytes(u16, packet_size, .big);
-
-        try response.append(@intFromEnum(self.header));
-        try response.appendSlice(size);
-        try response.append(self.code);
-        try response.append(self.sub_code);
-        try response.appendSlice(server_count);
-        try response.appendSlice(servers);
-
-        return try response.toOwnedSlice();
+        return servers;
     }
 };
--- src/packets/utils.zig
+++ src/packets/utils.zig
@@ -1,4 +1,7 @@
 const std = @import("std");
+const network = @import("network");
+const PacketType = @import("types.zig").PacketType;
+
 const mem = std.mem;
 const Endian = std.builtin.Endian;
 const allocator = std.heap.page_allocator;
@@ -16,4 +19,52 @@ pub fn write_into_bytes(comptime T: type, value: T, buffer: [@sizeOf(T)]u8) void
 
 pub fn block_alloc(T: type, size: usize) ![]T {
     return try allocator.alloc(T, size);
+}
+
+pub fn create_packet(
+    packet_type: PacketType,
+    code: u8,
+    sub_code: u8,
+    data: []const []const u8,
+) ![]const u8 {
+    const payload = try std.mem.concat(allocator, u8, data);
+    const header = try create_header(packet_type, payload.len, code, sub_code);
+    return try std.mem.concat(allocator, u8, &.{ header, payload });
+}
+
+pub fn create_header(
+    packet_type: PacketType,
+    payload_size: usize,
+    code: u8,
+    sub_code: u8,
+) ![]const u8 {
+    const length_size = get_length_size(packet_type);
+    const header_size: usize = 3 + length_size;
+    const size = payload_size + header_size;
+
+    var header = try block_alloc(u8, header_size);
+
+    header[0] = @intFromEnum(packet_type);
+
+    switch (length_size) {
+        1 => header[1] = @intCast(size),
+        2 => std.mem.copyForwards(
+            u8,
+            header[1 .. length_size + 1],
+            try split_into_bytes(u16, @intCast(size), .big),
+        ),
+        else => unreachable,
+    }
+
+    header[length_size + 1] = code;
+    header[length_size + 2] = sub_code;
+
+    return header;
+}
+
+pub fn get_length_size(packet_type: PacketType) usize {
+    return switch (packet_type) {
+        .C1, .C3 => 1,
+        .C2, .C4 => 2,
+    };
 }
